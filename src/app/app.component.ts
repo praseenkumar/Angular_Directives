613aa1905d340f365bd2c841f5ce4a212e3e1e0a
praseen kumar thatipally <praseenkumar1996@gmail.com>
2018-10-08 11:48:40 +0530

    Angular directives commited

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //viewMode = 'map';
+
+  courses;
+
+  loadCourses(){
+    this.courses = [
+      {id:1,name:'course1'},
+      {id:2,name:'course2'},
+      {id:3,name:'course3'}
+    ];
+    
+  }
+
+  trackCourse(index,course){
+   return course?course.id:undefined
+  }
+
+  /*
+  onAdd(){
+    this.courses.push({id:4,name:'course4'});
+  }
+  onRemove(course){
+    let index=this.courses.indexOf(course);
+    this.courses.splice(index,1);
+  }
+  onChange(course){
+    course.name='UPDATED';
+  }
+  */
   title = 'directives';
 }
